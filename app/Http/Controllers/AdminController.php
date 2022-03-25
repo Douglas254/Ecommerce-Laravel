181503c1dc1a9e63d9cad70255beab597c5c7cfc
Douglas254 <dobara@kabarak.ac.ke>
2022-03-25 13:54:11 +0300

    Show And Delete Data from database via Admin Dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -38,4 +38,21 @@ class AdminController extends Controller
 
         return redirect()->back()->with('message','Product Added Successfully');
     }
+
+    public function showproduct()
+    {
+        $data = product::all(); // store all the values from the product table to the data variable
+
+        return view('admin.showproduct',compact('data'));
+    }
+
+    public function deleteproduct($id)
+    {
+        $data = product::find($id);
+
+        $data->delete();
+
+        return redirect()->back()->with('message','Product Deleted');
+
+    }
 }
