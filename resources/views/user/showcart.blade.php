9bbe148eb0c2611ba09667c470f02f8061d0e2ea
Douglas254 <dobara@kabarak.ac.ke>
2022-03-26 09:05:08 +0300

    How To Show Product In Cart

--- resources/views/user/showcart.blade.php
+++ resources/views/user/showcart.blade.php
@@ -71,7 +71,7 @@ https://templatemo.com/tm-546-sixteen-clothing
                         @auth
 
                           <li class="nav-item">
-                            <a class="nav-link" href="{{'showcart'}}"> <i class="fa fa-shopping-cart"></i>Cart[{{$count}}]</a>
+                            <a class="nav-link" href="{{'showproduct'}}"> <i class="fa fa-shopping-cart"></i>Cart[{{$count}}]</a>
                            
                           </li>
 
@@ -106,100 +106,26 @@ https://templatemo.com/tm-546-sixteen-clothing
     
     </header>
 
-    <!-- Page Content -->
-    <!-- Banner Starts Here -->
-    <div class="banner header-text">
-      <div class="owl-banner owl-carousel">
-        <div class="banner-item-01">
-          <div class="text-content">
-            <h4>Best Offer</h4>
-            <h2>New Arrivals On Sale</h2>
-          </div>
-        </div>
-        <div class="banner-item-02">
-          <div class="text-content">
-            <h4>Flash Deals</h4>
-            <h2>Get your best products</h2>
-          </div>
-        </div>
-        <div class="banner-item-03">
-          <div class="text-content">
-            <h4>Last Minute</h4>
-            <h2>Grab last minute deals</h2>
-          </div>
-        </div>
-      </div>
+
+    <div style="padding: 100px;" align="center">
+        <table>
+            <tr style="background-color: gray;">
+                <td style="padding: 10px; font-size: 20px; color: cyan">Product Name</td>
+                <td style="padding: 10px; font-size: 20px; color: cyan">Quantity</td>
+                <td style="padding: 10px; font-size: 20px; color: cyan">Price</td>
+            </tr>
+
+            @foreach($cart as $carts)
+
+              <tr style="background-color: black">
+                <td style="padding: 10px; color: white">{{$carts->product_title}}</td>
+                <td style="padding: 10px; color: white">{{$carts->quantity}}</td>
+                <td style="padding: 10px; color: white">{{$carts->price}}</td>
+              </tr>
+
+            @endforeach
+        </table>
     </div>
-    <!-- Banner Ends Here -->
-
-        @include('user.product')
-
-    <div class="best-features">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="section-heading">
-              <h2>About Sixteen Clothing</h2>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="left-content">
-              <h4>Looking for the best products?</h4>
-              <p><a rel="nofollow" href="https://templatemo.com/tm-546-sixteen-clothing" target="_parent">This template</a> is free to use for your business websites. However, you have no permission to redistribute the downloadable ZIP file on any template collection website. <a rel="nofollow" href="https://templatemo.com/contact">Contact us</a> for more info.</p>
-              <ul class="featured-list">
-                <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-                <li><a href="#">Consectetur an adipisicing elit</a></li>
-                <li><a href="#">It aquecorporis nulla aspernatur</a></li>
-                <li><a href="#">Corporis, omnis doloremque</a></li>
-                <li><a href="#">Non cum id reprehenderit</a></li>
-              </ul>
-              <a href="about.html" class="filled-button">Read More</a>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="right-image">
-              <img src="assets/images/feature-image.jpg" alt="">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-    <div class="call-to-action">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="inner-content">
-              <div class="row">
-                <div class="col-md-8">
-                  <h4>Creative &amp; Unique <em>Sixteen</em> Products</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis amet elite author nulla.</p>
-                </div>
-                <div class="col-md-4">
-                  <a href="#" class="filled-button">Purchase Now</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="inner-content">
-              <p>Copyright &copy; 2020 Sixteen Clothing Co., Ltd.
-            
-            - Design: <a rel="nofollow noopener" href="https://templatemo.com" target="_blank">TemplateMo</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
 
 
     <!-- Bootstrap core JavaScript -->
