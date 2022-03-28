1f213e6c71cf6ac4d5b62847e28de437bd285d43
Douglas254 <dobara@kabarak.ac.ke>
2022-03-28 15:10:14 +0300

    debug terminal error

--- config/database.php
+++ config/database.php
@@ -70,7 +70,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => $DATABASE_URL['host'],
             'port' => $DATABASE_URL['port'],
-            'database' => ltrim($DATABASE_URL['path'], charlist:"/"),
+            'database' => ltrim($DATABASE_URL['path'],"/"),
             'username' => $DATABASE_URL['user'],
             'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
